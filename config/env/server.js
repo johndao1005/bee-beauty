b8691e64edc305b5deabc84e9201dfe7efcf539a
johndao1005 <john.d1729@gmail.com>
2022-09-27 13:07:04 +1300

    Adding the first page fields

--- config/env/server.js
+++ config/env/server.js
@@ -3,6 +3,5 @@ module.exports = ({ env }) => ({
   proxy: true,
   app:{
     keys:env.array('APP_KEYS',['q8zO4v1C7UvalgAFjwoyBg==,ngd7fP27Kku+uXziddlHHA==,4njcqfobHf/aFQ9q4pp0zg==,wmXJPtc3R125XXECWmM14g==']),
-
   }
 });
